470597b1ec75a8da30b994bc25b015acdf01bbbc
Márcio Torres <marciojrtorres@gmail.com>
2014-07-21 21:54:17 -0300

    cobaia, projeto inicial, intro

---
+++ cobaia/Cobaia.java
@@ -0,0 +1,23 @@
+
+public class Cobaia {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Cobaia sobreviveu à experiência");
+
+		double soma = 0;
+
+		for (int i = 0; i < 10; i++) {
+			soma = soma + 0.1;
+		}
+
+		System.out.println("soma == 1: " + (soma == 1));
+
+		System.out.println();
+
+		System.out.println(soma);
+
+		// Princípio da Menor Surpresa
+
+	}
+}
