6cfb42986bff0286a6601ee6238ae5fdca41dbf0
Marcio Ramos <marciojrtorres@gmail.com>
2014-09-15 22:26:49 -0300

    aula decorator

--- cobaia/Main.java
+++ cobaia/Main.java
@@ -1,10 +1,53 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("teste");
-        System.out.println("aula");
-        System.out.println("curso");
-        System.out.println("java");
+        ComputadorQueConta c = new ComputadorQueConta(new Computador());
+        System.out.println(c.getEstado()); // desligado
+        usa(c);
+        System.out.println(c.getEstado()); // ?
+        c.reseta();
+        // quantas vezes o botao liga foi pressionado
+        System.out.println(c.getVezesLigado()); // 3
 
     }
+
+    private static void usa(Computador c) {
+        c.liga();
+        c.liga();
+        c.liga();
+    }
+}
+
+// Decorator
+class ComputadorQueConta extends Computador {
+
+    // trocando herança por composição
+    private Computador decorado; // estendido/decorado
+    private int cont;
+
+    public ComputadorQueConta(Computador computador) {
+        this.decorado = computador;
+    }
+
+    public void liga() {
+        decorado.liga();
+        cont++;
+    }
+
+    public void desliga() {
+        decorado.desliga();
+    }
+
+    public void reseta() {
+        decorado.reseta();
+    }
+
+    public String getEstado() {
+        return decorado.getEstado();
+    }
+
+    public int getVezesLigado() {
+        return cont;
+    }
+
 }
